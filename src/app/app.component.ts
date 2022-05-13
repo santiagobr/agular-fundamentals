daf22d52a75ac007cad8b8cf4f96e40d5ab14eee
santiagobr <santiagobastidasrodriguez@gmail.com>
2022-05-13 05:45:02 -0500

    First angular classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Santiago';
+  age = 20;
+  img =
+    'https://static.platzi.com/media/achievements/badge-fundamentos-angular-1e2e6052-abba-4c05-9233-03bba9866523.png';
+  btnDisabled = true;
+
+  person = {
+    name: 'Santiago',
+    age: 20,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png',
+  };
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
