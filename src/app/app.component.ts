ac7462ed5b823c7aceb1789353e11cb6a017576b
santiagobr <santiagobastidasrodriguez@gmail.com>
2022-05-17 13:49:26 -0500

    *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,9 @@ export class AppComponent {
     avatar: 'https://www.w3schools.com/howto/img_avatar.png',
   };
 
+  names: string[] = ['Nico', 'Juli', 'Santi'];
+  newName = '';
+
   toggleButton() {
     this.btnDisabled = !this.btnDisabled;
   }
@@ -34,5 +37,14 @@ export class AppComponent {
   changeName(event: Event) {
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
+  }
+
+  addName() {
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number) {
+    this.names.splice(index, 1)
   }
 }
