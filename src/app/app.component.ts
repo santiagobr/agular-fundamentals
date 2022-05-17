09a20b480b77d35192d44d126221e57c8819b38b
santiagobr <santiagobastidasrodriguez@gmail.com>
2022-05-17 17:03:27 -0500

    *ngFor Arrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Product } from './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,6 +20,40 @@ export class AppComponent {
 
   names: string[] = ['Nico', 'Juli', 'Santi'];
   newName = '';
+
+  products: Product[] = [
+    {
+      name: 'El mejor juguete',
+      price: 565,
+      image: '../assets/images/toy.jpg',
+      category: 'Toys',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: '../assets/images/bike.jpg',
+    },
+    {
+      name: 'Colección de albums',
+      price: 34,
+      image: '../assets/images/album.jpg',
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: '../assets/images/books.jpg',
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: '../assets/images/house.jpg',
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: '../assets/images/glasses.jpg',
+    },
+  ];
 
   toggleButton() {
     this.btnDisabled = !this.btnDisabled;
@@ -45,6 +79,6 @@ export class AppComponent {
   }
 
   deleteName(index: number) {
-    this.names.splice(index, 1)
+    this.names.splice(index, 1);
   }
 }
